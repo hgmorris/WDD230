7ed4ea78a5f66768f3fd2916146f9d63ddbb79dc
Your Name <hinnehmorris4@gmail.com>
2023-05-30 11:34:37 -0600

    changes

--- chamber/scripts/base.js
+++ chamber/scripts/base.js
@@ -1,7 +1,19 @@
-function toggleMenu(){
-document.querySelector(".menu").classList.toggle('active');
-document.querySelector("#menu-closed").classList.toggle('active');
-document.querySelector("#menu-open").classList.toggle('active');
-    }
+
+let newDateChamber = new Date()
+
+// function toggleMenu(){
+// document.querySelector(".menu").classList.toggle('active');
+// document.querySelector("#menu-closed").classList.toggle('active');
+// document.querySelector("#menu-open").classList.toggle('active');
+//     }
     
-    document.querySelector("#hamburger-menu").addEventListener('click', toggleMenu);
+//     document.querySelector("#hamburger-menu").addEventListener('click', toggleMenu);
+
+
+let message = document.querySelector("#message");
+console.log(message)
+if(newDateChamber.getDay() == 1 || newDateChamber.getDay() == 2) {
+    message.style.display = "block"
+} else {
+    message.style.display = "none"
+}
--- lesson02/scripts/main.js
+++ lesson02/scripts/main.js
@@ -5,3 +5,5 @@ let currentDate = document.lastModified;
 
 document.querySelector('.year-name').innerHTML = '&copy;' + year + " Morris Hinneh - Idaho";
 document.querySelector('.last-modified').innerHTML = " Last Modified: " + currentDate;
+
+
