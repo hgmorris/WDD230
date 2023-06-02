873336e2247a1ea74f2f26d97c7a050d1d110464
Your Name <hinnehmorris4@gmail.com>
2023-06-02 16:18:19 -0600

    changes

--- chamber/scripts/base.js
+++ chamber/scripts/base.js
@@ -1,19 +1,25 @@
 
-let newDateChamber = new Date()
+const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+let newDateChamber = new Date().toLocaleDateString('en-us', options)
 
-// function toggleMenu(){
-// document.querySelector(".menu").classList.toggle('active');
-// document.querySelector("#menu-closed").classList.toggle('active');
-// document.querySelector("#menu-open").classList.toggle('active');
-//     }
+function toggleMenu(){
+document.querySelector(".menu").classList.toggle('active');
+document.querySelector("#menu-closed").classList.toggle('active');
+document.querySelector("#menu-open").classList.toggle('active');
+    }
     
-//     document.querySelector("#hamburger-menu").addEventListener('click', toggleMenu);
+    document.querySelector("#hamburger-menu").addEventListener('click', toggleMenu);
 
 
 let message = document.querySelector("#message");
+message.innerHTML = newDateChamber
 console.log(message)
-if(newDateChamber.getDay() == 1 || newDateChamber.getDay() == 2) {
-    message.style.display = "block"
-} else {
-    message.style.display = "none"
-}
+
+let text = document.lastModified;
+document.querySelector("#Modification").textContent = text
+
+// if(newDateChamber.getDay() == 1 || newDateChamber.getDay() == 2) {
+//     message.style.display = "block"
+// } else {
+//     message.style.display = "none"
+// }
